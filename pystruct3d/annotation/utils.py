4e217c51fe2a5b88d52f031809d984738583e0f6
kaufmann <fabian.kaufmann@rptu.de>
2024-09-20 17:47:31 +0200

    parse ifc to demo onotology

--- pystruct3d/annotation/utils.py
+++ pystruct3d/annotation/utils.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 import numpy as np
+import e57
 
 from pystruct3d.utils import las_utils
 
@@ -19,11 +20,14 @@ def transfer_labels(
     # find nearest neighbour
     print("Transfer labels ...")
     for ix, point in enumerate(labeled_pcd.points):
-        _, idx, _ = unlabeled_tree.search_hybrid_vector_3d(point, 0.2, 1)
-        idx_arr = np.asarray(idx)
+        _, idx, _ = unlabeled_tree.search_hybrid_vector_3d(point, 0.03, 100)
+        # _, idx, _ = unlabeled_tree.search_knn_vector_3d(point, 1)
         # transfer label to new array
-        if idx_arr is not None:
-            new_labels[idx_arr[0]] = labels_array[ix]
+        if ix % 1000000 == 0:
+            print(f"{ix:,}/{np.shape(labels_array)[0]:,} transferred ...")
+        if idx:
+            idx_arr = np.asarray(idx)
+            new_labels[idx_arr] = labels_array[ix]
 
     return new_labels
 
@@ -33,27 +37,49 @@ def main():
     # labeled_pcd = o3d.io.read_point_cloud("/home/kaufmann/Desktop/labeled.pcd")
 
     # for LAS point clouds and numpy labels file
-    labels_arr = np.load("/home/kaufmann/Desktop/labels.npy")
-    labeled_pcd = las_utils.read_las_file("/home/kaufmann/Desktop/labeled.las")
-    unlabeled_pcd = las_utils.read_las_file("/home/kaufmann/Desktop/unlabeled.las")
-
-    # # when you use an ascii cloud that contains the labels you may use this code
-    # # just uncomment an comment the code above
-    # labeled_arr = np.loadtxt("/home/kaufmann/Desktop/labeled.txt")
-    # labeled_points = labeled_arr[:, 0:3]
-    # labeled_colors = labeled_arr[:, 3:6]
-    # labels_arr = labeled_arr[:, 6].flatten()
-    # # combine arrays to open3d point cloud
-    # labeled_pcd = o3d.geometry.PointCloud()
-    # labeled_pcd.points = o3d.utility.Vector3dVector(labeled_points)
-    # labeled_pcd.colors = o3d.utility.Vector3dVector(labeled_colors / 255)
-    # # open unlabeled point cloud
+    # labels_arr = np.load("/home/kaufmann/Desktop/labels.npy")
+    # labeled_pcd = las_utils.read_las_file("/home/kaufmann/Desktop/labeled.las")
     # unlabeled_pcd = las_utils.read_las_file("/home/kaufmann/Desktop/unlabeled.las")
 
-    new_labels = transfer_labels(labeled_pcd, labels_arr, unlabeled_pcd)
+    # when you use an ascii cloud that contains the labels you may use this code
+    # just uncomment and comment the code above
+    input_file = (
+        "/home/kaufmann/Desktop/ADAC/20230812_ADAC_1_aligned_labeled_merged.asc"
+    )
+    input_arr = np.loadtxt(
+        input_file,
+        dtype=np.float32,
+    )
+    labeled_points = input_arr[:, 0:3]
+    labeled_colors = input_arr[:, 3:6]
+    labels_arr = input_arr[:, 6].flatten()
+    print(f"Old labels: {np.unique(labels_arr)}")
+    # combine arrays to open3d point cloud
+    labeled_pcd = o3d.geometry.PointCloud()
+    labeled_pcd.points = o3d.utility.Vector3dVector(labeled_points)
+    labeled_pcd.colors = o3d.utility.Vector3dVector(labeled_colors / 255)
+    # o3d.visualization.draw_geometries([labeled_pcd])
 
-    ascii_pcd = np.hstack((np.asarray(unlabeled_pcd.points), new_labels.reshape(-1, 1)))
-    np.savetxt("/home/kaufmann/Desktop/transfered_labels.txt", ascii_pcd)
+    # open unlabeled point cloud
+    # unlabeled_pcd = las_utils.read_las_file("/home/kaufmann/Desktop/unlabeled.las")
+    unlabeled_file = "/home/kaufmann/Desktop/ADAC/20230727_ADAC_1_aligned.e57"
+    unlabeled = e57.read_points(unlabeled_file)
+    unlabeled_pcd = o3d.geometry.PointCloud()
+    unlabeled_pcd.points = o3d.utility.Vector3dVector(unlabeled.points)
+    unlabeled_pcd.colors = o3d.utility.Vector3dVector(unlabeled.color)
+    print(f"Unlabeled {unlabeled_pcd}")
+    print("Transferring labels ...")
+    new_labels = transfer_labels(labeled_pcd, labels_arr, unlabeled_pcd)
+    print(f"New labels: {np.unique(new_labels)}")
+
+    ascii_pcd = np.hstack(
+        (
+            np.asarray(unlabeled_pcd.points, dtype=np.float32),
+            np.asarray(unlabeled_pcd.colors, dtype=np.float32),
+            new_labels.reshape(-1, 1),
+        )
+    )
+    np.savetxt(f"{unlabeled_file[:-4]}_transfered_labels.txt", ascii_pcd, fmt="%.8e")
 
 
 if __name__ == "__main__":
---
+++ pystruct3d/ransac/fit_line.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def get_seed_points(points, number_seeds):
+
+    # pick random points
+
+    # nearest neigbour search
+
+    # additional condition: points with similar normals
+
+    pass
+
+
+def estimate_line(seed_points):
+
+    pass
+
+
+def find_inliers(line):
+
+    pass
+
+
+def fit_line(points):
+    pass
